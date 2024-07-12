5432b5851d5c7926168b0deacf673253189c7de9
Kelvin Chung <79961717+kc89878@users.noreply.github.com>
2024-07-12 14:45:25 +0800

    Update netflix_analysis.sql

--- netflix_analysis.sql
+++ netflix_analysis.sql
@@ -10,8 +10,13 @@ inner join netflix_directors nd on n.show_id=nd.show_id
 group by nd.director
 having COUNT(distinct n.type) > 1
 
+--> Content Curation
+/* By understanding which directors have created a successful mix of both movies and TV shows, 
+   Netflix can collaborate with these directors for exclusive content creation. 
+   This can attract a wider audience and drive subscriptions. */
+     
 ------------------------------------------------------------------------------------------------------------------------
---2. Which five country has highest number of comedy movies? 
+--2. Which five countries has highest number of comedy movies? 
 select top 5 nc.country , COUNT(distinct ng.show_id) as no_of_movies
 from netflix_genre ng
 inner join netflix_country nc on ng.show_id = nc.show_id
@@ -19,6 +24,11 @@ inner join netflix n on ng.show_id = nc.show_id
 where ng.genre = 'Comedies' and n.type = 'Movie'
 group by  nc.country
 order by no_of_movies desc
+
+--> Localization
+/* Identifying the top five countries with the highest number of comedy movies 
+   can help Netflix tailor its content strategy for these regions. 
+   They can invest in producing more localized comedy content to cater to specific audience preferences. */
 
 ------------------------------------------------------------------------------------------------------------------------
 --3. For each year (as per date added to netflix), which director has maximum number of movies released?
@@ -36,6 +46,10 @@ from cte
 )
 select * from cte2 where rn = 1
 
+--> Release Strategy
+/* Knowing which directors have consistently released movies each year can help Netflix plan their content release calendar strategically. 
+   By partnering with prolific directors, Netflix can ensure a steady flow of engaging content for subscribers. */
+
 ------------------------------------------------------------------------------------------------------------------------
 --4. What is average duration of movies in each genre?
 select ng.genre , avg(cast(REPLACE(duration,' min','') AS int)) as avg_duration
@@ -45,6 +59,10 @@ where type = 'Movie'
 group by ng.genre
 order by avg_duration
 
+--> Optimized Content Length
+/* Understanding the average duration of movies in each genre can guide Netflix in producing content that aligns with viewer preferences. 
+   This data can help in optimizing content length to keep audiences engaged and satisfied. */
+     
 ------------------------------------------------------------------------------------------------------------------------
 --5. Find the list of directors who have created horror and comedy movies both?
 --   Display director names along with number of comedy and horror movies directed by them 
@@ -57,4 +75,12 @@ inner join netflix_directors nd on n.show_id = nd.show_id
 where type = 'Movie' and ng.genre in ('Comedies','Horror Movies')
 group by nd.director
 having COUNT(distinct ng.genre) = 2;
+
+--> Genre Diversification
+/* Identifying directors who have created both horror and comedy movies can pave the way for Netflix to explore crossover genres. 
+   Collaborating with such versatile directors can lead to innovative content that caters to diverse audience tastes. */
+
 ------------------------------------------------------------------------------------------------------------------------
+---  Conclusion ---
+/* Overall, the insights from the analysis can help Netflix make informed decisions regarding content creation, curation, and audience engagement. 
+   By leveraging these insights effectively, Netflix can enhance its content library, attract a broader audience, and ultimately increase profitability.
